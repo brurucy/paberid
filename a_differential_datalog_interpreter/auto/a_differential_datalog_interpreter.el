de86374c465c44b002a9129b725d065d20bc66b0
brurucy <brurucy@gmail.com>
2023-05-27 17:46:38 +0200

    Algorithms are pretty now

---
+++ a_differential_datalog_interpreter/auto/a_differential_datalog_interpreter.el
@@ -0,0 +1,40 @@
+(TeX-add-style-hook
+ "a_differential_datalog_interpreter"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("acmart" "sigconf" "screen" "review" "natbib")))
+   (TeX-add-to-alist 'LaTeX-provided-package-options
+                     '(("algorithm2e" "ruled" "vlined" "linesnumbered")))
+   (TeX-run-style-hooks
+    "latex2e"
+    "acmart"
+    "acmart10"
+    "amsmath"
+    "amssymb"
+    "multirow"
+    "microtype"
+    "tikz"
+    "tikz-qtree"
+    "subfig"
+    "algorithm2e"
+    "libertine")
+   (TeX-add-symbols
+    "BibTeX")
+   (LaTeX-add-labels
+    "ex1"
+    "exsne"
+    "ex6"
+    "ex7"
+    "fig:substitution_simple_ddflow"
+    "fig:substitution_indexed_ddflow"
+    "program:rhodfs"
+    "program:rhodfss"
+    "tab:datasets"
+    "tab:mainbenchmark"
+    "tab:memorybenchmark")
+   (LaTeX-add-environments
+    "exmp")
+   (LaTeX-add-bibliographies
+    "software"))
+ :latex)
+
